bff72eda74a7bee8af9f63c3cc64a38c37973a42
Amil Harshak v k <amilvk125@gmail.com>
2021-01-07 19:34:22 +0530

    Add files via upload

--- diceroll.py
+++ diceroll.py
@@ -3,13 +3,15 @@ dice = ["-","-","-",
         "-","-","-","-"]
 
 def display_dice():
-    number = random.randint(0,6)        
+    number = random.randint(1,6)        
     print(" " + dice[0] + dice[1] + dice[2] + dice[3] + dice[4] + dice[5] + dice[6])
     print("|" + " " + " " + " " + " " + " " + " " + " " + "|")
     print("|" + " " + " "  , number , " " + " " + "|")
     print("|" + " " + " " + " " + " " + " " + " " + " " + "|")
     print(" " + dice[0] + dice[1] + dice[2] + dice[3] + dice[4] + dice[5] + dice[6])
-
-Roll_the_dice = input('Enter "roll" or "Roll" to Roll the Dice : ')
-if Roll_the_dice == "roll" or "Roll":
-    display_dice()
+def dice_roll():
+    Roll_the_dice = "yes"
+    while Roll_the_dice == "yes" or "y":
+        display_dice()
+        Roll_the_dice = input('Enter "yes" or "y" to Roll the Dice : ')
+dice_roll()    
